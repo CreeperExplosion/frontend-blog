7b1f41555adaf13bd75b7706c7a2ec27f16c8bfb
CreeperExplosion <ghibranresearch@gmail.com>
2020-06-11 10:17:34 +0200

    unused footer

---
+++ src/component/Foot.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+const Styles = styled.div`
+    {
+        padding-top : 4em;
+    }
+`
+
+export default function Foot() {
+    return (
+        <Styles>
+        </Styles>
+    )
+}
